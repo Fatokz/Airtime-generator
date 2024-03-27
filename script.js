4c3f32be72993245fc385800cb49c776afb7c584
FATOKUN EMMANUEL <fatokunemmanuel16@gmail.com>
2024-03-27 06:16:24 +0100

    table

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
+let copys = document.getElementById('copy')
+let paste = document.getElementById('paste')
 let out = document.getElementById("show");
 let docNum = document.getElementById("select");
 let docNumb = document.getElementById("selectt");
 let loadd = document.getElementById("enter");
+let table = document.getElementById("shownetwork");
 let airtime;
 let allPins = []
-console.log(out.innerHTML);
-// out.innerHTML = Math.floor(Math.random() *10000000000);
+
+// copys.style.display = "initial"
+// paste.style.display = "none"
+
 
 function generate() {
     out.innerHTML = ""
     // let out = document.getElementById("show")
-    if (docNum.value == '' ) {
+    if (docNum.value == '') {
         alert("Select a Network ")
-    }else if (docNumb.value == ''){
+    } else if (docNumb.value == '') {
         alert("Select an amount")
-    } 
+    }
     else {
         for (let index = 1; index <= 16; index++) {
             airtime = Math.floor(Math.random() * 10)
             out.innerHTML += airtime;
         }
         console.log(out.innerHTML);
+        let date = new Date()
+        let pins = {
+            pin: out.innerHTML,
+            network: docNum.value,
+            amount: docNumb.value,
+            date: `${date.toDateString()}`
+        }
+        console.log(pins);
+        allPins.push(pins)
+        console.log(allPins);
     }
-    out.innerHTML += `<button id="copy" onclick = "copy()"><i class="fa-regular fa-clipboard"></i></button>`
+
+    table.innerHTML = ''
+    for (let index = 0; index < allPins.length; index++) {
+        const element = allPins[index];
+        console.log(element);
+        table.innerHTML += `
+            <tr>
+                <td>${element.network}</td>
+                <td>${element.amount}</td>
+                <td>${element.pin}</td>
+                <td>${element.date}</td>
+                <td>${'<button id="copys" onclick = "copy()"> <i class="fa-regular fa-clipboard"></i> </button>'}</td>
+            </tr>
+        `
+    }
 }
 
 function load() {
+    console.log(airtime);
     if (loadd.value == '') {
         alert("You haven't enter your airtime yet")
     }
